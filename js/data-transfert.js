a4933020647fa3dc4512b217f392b529047cb13e
Franck Boinay <franck.boinay@gmail.com>
2016-11-17 09:12:49 +0100

    Display files with informations (name, size, transfert type)

--- js/data-transfert.js
+++ js/data-transfert.js
@@ -1,1 +1,10 @@
-angular.module("data-transfert", []);
+angular.module("data-transfert", [])
+
+	.controller("FilesController", ["$scope", "$http", "ConfigService",
+		function ($scope, $http, ConfigService) {
+		$scope.files = [];
+
+		$http.get(ConfigService.Url).success(function (data) {
+			$scope.files = data;
+		});
+	}]);
--- main.js
+++ main.js
@@ -1,1 +1,70 @@
-angular.module("app", ["data-transfert"]);
+angular.module("app", ["data-transfert"])
+
+	.config(["$httpProvider", "DemoDataProvider", function ($httpProvider, DemoDataProvider) {
+		
+		var interceptor = ["$rootScope", "$q", "$location", "ConfigService",
+			function ($rootScope, $q, $location, ConfigService) {
+
+			var demoData = DemoDataProvider.$get();
+
+			return {
+				'request': function (request) {
+					if (request.url.indexOf(ConfigService.Url) > -1) {
+						request.timeout = 1;
+					}
+					return request;
+				},
+				'responseError': function (response) {
+					if (response.config.url.indexOf(ConfigService.Url) > -1) {
+						response.data = demoData.getFiles();
+						response.status = 200;
+					}
+					return response;
+				}
+			};
+
+		}];
+
+		$httpProvider.interceptors.push(interceptor);
+	}])
+	
+	.provider("DemoData", function DemoDataProvider() {
+		function DemoData() {
+			this.getFiles = function () {
+				return [
+					{
+						"name": "DICOM_patientXY_1.dcm",
+						"size": "1.5 MB",
+						"transfertType": "Upload",
+						"status": "Queued"
+					},
+					{
+						"name": "DICOM_patientXY_2.dcm",
+						"size": "1.7 MB",
+						"transfertType": "Upload",
+						"status": "Queued"
+					},
+					{
+						"name": "VSD.Thorax.089Y.M.CT.7.000.dcm.zip",
+						"size": "2 GB",
+						"transfertType": "Download",
+						"status": "Queued"
+					},
+					{
+						"name": "NIFTI_patientXY.nii",
+						"size": "1.2 MB",
+						"transfertType": "Upload",
+						"status": "Queued"
+					}
+				];
+			};
+		};
+
+		this.$get = function demoDataFactory() {
+			return new DemoData();
+		};
+	})
+	
+	.service("ConfigService", function () {
+		this.Url = "https://localhost/data-transfert/files";
+	});
