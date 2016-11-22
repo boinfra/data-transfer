c0d7de3ab12ea05ddd1cc85e3cb9b035685e974d
Franck Boinay <franck.boinay@gmail.com>
2016-11-22 11:11:26 +0100

    Drop files and get names, contents and count

--- js/drop.js
+++ js/drop.js
@@ -1,18 +1,21 @@
+var files = { names: [], contents: [], count: 0 };
+
 function allowDrop(ev) {
 	ev.preventDefault();
 }
 
 function drop(ev) {
 	ev.preventDefault();
-	var files = ev.dataTransfer.files;
-	for (var i = 0; i < files.length; i++) {
-		var file = files[i];
+	for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+		var file = ev.dataTransfer.files[i];
+		files.names[i] = file.name;
 		var reader = new FileReader();
 		reader.readAsText(file);
-		reader.onload = (function (theFile) {
-			return function (e) {
-				console.debug(e.target.result);
-        };
-      })(file);
+		reader.onload = function (e) {
+			files.contents[files.count] = e.target.result;
+			console.debug(files.names[files.count]);
+			console.debug(files.contents[files.count]);
+			files.count++;
+		};
 	}
 }
