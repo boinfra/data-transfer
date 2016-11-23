4ba46f2373dfb786e0d9feb1505838cdaa352a71
Franck Boinay <franck.boinay@gmail.com>
2016-11-23 11:39:52 +0100

    Browser detection, display message depending to the browser and detect dropped elements depending on browser

--- js/drop.js
+++ js/drop.js
@@ -1,3 +1,25 @@
+// Cross browser support
+var isChromium = window.chrome,
+	chrome,
+	winNav = window.navigator,
+	vendorName = winNav.vendor,
+	isOpera = winNav.userAgent.indexOf("OPR") > -1,
+	isIEedge = winNav.userAgent.indexOf("Edge") > -1,
+	isIOSChrome = winNav.userAgent.match("CriOS"),
+	message = document.getElementById("dropMessage");
+
+if (isIOSChrome) {
+	message.innerHTML = "Drag n'drop your files or folders here";
+	chrome = true;
+} else if (isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isIEedge == false) {
+	message.innerHTML = "Drag n'drop your files or folders here";
+	chrome = true;
+} else {
+	message.innerHTML = "Drag n'drop your files here";
+	chrome = false;
+}
+
+// Drop
 var files = { names: [], contents: [], count: 0 };
 
 function allowDrop(ev) {
@@ -6,16 +28,30 @@ function allowDrop(ev) {
 
 function drop(ev) {
 	ev.preventDefault();
-	for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-		var file = ev.dataTransfer.files[i];
-		files.names[i] = file.name;
-		var reader = new FileReader();
-		reader.readAsText(file);
-		reader.onload = function (e) {
-			files.contents[files.count] = e.target.result;
-			console.debug(files.names[files.count]);
-			console.debug(files.contents[files.count]);
-			files.count++;
-		};
+	var droppedFiles = chrome ? ev.dataTransfer.items : ev.dataTransfer.files;
+	if (chrome) {
+		for (var i = 0; i < droppedFiles.length; i++) {
+			var entry = droppedFiles[i].webkitGetAsEntry();
+			if (entry.isFile) {
+				console.debug("file");
+			}
+			else if (entry.isDirectory) {
+				console.debug("directory");
+			}
+		}
+	}
+	else {
+		for (var i = 0; i < droppedFiles.length; i++) {
+			var file = droppedFiles[i];
+			files.names[i] = file.name;
+			var reader = new FileReader();
+			reader.readAsText(file);
+			reader.onload = function (e) {
+				files.contents[files.count] = e.target.result;
+				console.debug(files.names[files.count]);
+				console.debug(files.contents[files.count]);
+				files.count++;
+			};
+		}
 	}
 }
